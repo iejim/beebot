65b3deeb19d6db525a0e68b717dfae8a8ff11fa8
Ivan Jimenez <ivan.jimenez@intec.edu.do>
2023-03-21 19:10:08 -0400

    Actualizar nodo PCA para Full ON/OFF, y dirección.

--- src/beebot_control/beebot_control/interfaz_pca.py
+++ src/beebot_control/beebot_control/interfaz_pca.py
@@ -10,7 +10,7 @@ from std_msgs.msg import String,Float32,Int32
 # Se necesitan ambos
 from beebot_msgs.msg import CanalPCA, CanalesPCA
 
-from pca9685_driver import Device
+from pca9685_driver import Device, DeviceException
 
 class InterfazPCA(Node):
 
@@ -23,16 +23,28 @@ class InterfazPCA(Node):
         # self.pub_avanzar = self.create_publisher(Float32, 'avanzar', 10)
 
         self.sub_pasos = self.create_subscription(
-            CanalesPCA,
-            'comando_pca',
-            self.leer_comando,
+            CanalesPCA,  # Tipo de mensaje
+            'comando_pca', # Nombre del /topico (canal) donde publicaremos
+            self.leer_comando, # Función callback para manejar el mensaje
             1)
+            
         self.pca = None
+
+        self.declare_parameter('direccion_i2c', 0x40) # Valor por defecto de la dirección i2c
+
         
 
-    def inicializar_pca(self, dev=0x40, freq=None):
+    def inicializar_pca(self, freq=None): # dev=None
         # Inicializa la conexión el PCA
-        self.pca = Device(dev) # TODO: Revisar si fue satisfactorio 
+        
+        d = self.get_parameter('direccion_i2c').get_parameter_value().integer_value
+        try:
+            self.logger("Registrando el controlador del PCA en la dirección: {}".format(d))
+            self.pca = Device(d) # TODO: Revisar si fue satisfactorio 
+        except DeviceException as e:
+            self.logger("Error abriendo el dispositivo I2C: {}".format(d))
+            self.destroy_node() # debería evitar seguir trabajando
+            return
 
         if freq is None:
             self.pca.set_pwm_frequency(50)
@@ -40,14 +52,16 @@ class InterfazPCA(Node):
 
         self.pca.set_pwm_frequency(freq)
         
+    def get_conectado(self):
+        return not self.pca is None
 
     # Se corre cada vez que llega un mensaje
     # ("reacciona" a los mensajes)
     def leer_comando(self,msg):
         if self.pca is None:
             # Entramos aquí si se llamó a spin() y nunca se inicializó el PCA.
-            self.logger("PCA: No se inicializó el dispositivo. Saliendo.")
-            self.destroy_node() # debería funcionar
+            self.logger("PCA: No se inicializó el dispositivo I2C. Saliendo.")
+            self.destroy_node() # debería evitar seguir trabajando
             return
         #msg es de tipo CanalesPCA()
         for canal in msg.lista:
@@ -57,11 +71,38 @@ class InterfazPCA(Node):
         self.get_logger().info(texto)
 
     def enviar_pwm(self, canal, valor):
-        valor = min(max(0,valor),4096) # En realidad, la librería de PCA ya hace esto
+        valor = min(max(0,valor),4096) # En realidad, la librería de PCA ya hace esto (hasta 4095)
 
-        self.pca.set_pwm(canal, valor)
+        if valor < 4096: # Recibe un valor normal
+            self.pca.set_pwm(canal, valor)
+        elif valor == 0: # Es raro un cero, así que lo forzamos
+            self.full_off(canal)
+        else: # Si no es porque pidieron 4096, así que forzamos el ON
+            self.full_on(canal) 
         # Pudiera publicarse para poder grabarse en un bag
         self.logger("PCA: (%s, %s)" %(canal, valor))
+    
+    def full_off(self, canal):
+        self.__check_range('led_number', canal)
+        registro_OFF_High = self.pca.calc_led_register(canal) #    apunta a LEDx_OFF_L
+        self.pca.write(registro_OFF, 0x00) # Reinicia cualquier timer
+        self.pca.write(registro_OFF + 1, 0x10) # Fuerza el pin a OFF (escriba a LEDx_OFF_H)
+        self.pca.write(registro_OFF - 1, 0x00) # Deshabilita forzar el pin a ON (escriba a LEDx_ON_H)
+        
+    def full_on(self, canal):
+        self.__check_range('led_number', canal)
+        register_ON_High = self.pca.calc_led_register(canal)-1 # apunta a LEDx_ON_H
+        self.pca.write(register_ON_High, 0x10) # Fuerza el pin a ON
+        
+    def __check_led_range(self, type, value):
+        range = (0, 15)
+        # range = self.ranges[type]
+        if value < range[0]:
+            raise DeviceException("%s debe ser mayor que %s, recibí %s" % (type, range[0], value))
+        if value > range[1]:
+            raise DeviceException("%s debe ser menor que %s, recibí %s" % (type, range[1], value))
+
+        
 
 
 
@@ -79,12 +120,19 @@ def main(args=None):
     nodo.inicializar_pca() 
 
     # En este caso solo espera a que llegue un mensaje y reaccionar
-    rclpy.spin(nodo) 
+    if nodo.get_conectado(): 
+        try:
+            rclpy.spin(nodo) 
+        except KeyboardInterrupt:
+            pass
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    nodo.destroy_node() # Buena práctica
+    try:
+        nodo.destroy_node() # Buena práctica
+    except rclpy.handle.InvalidHandle:
+        pass
     rclpy.shutdown() # Desconecta el prorama de ROS antes de cerrar
 
 
